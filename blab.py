2f3b04c107e6a36e2d3fcf175d0f6c3724f82583
DavidKnacker <daknacker@gmail.com>
2016-09-22 14:16:38 +0200

    first fix

--- blab.py
+++ blab.py
@@ -3,12 +3,12 @@ import pyhdb
 import time
 
 UDP_IP = "10.178.202.130"
-UDP_PORT = 5000
+UDP_PORT = 5001
 MESSAGE = "hallo"
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-
+sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP,UDP_PORT))
 connection = pyhdb.connect(
     host="10.178.202.133",
     port=30015,
@@ -17,13 +17,15 @@ connection = pyhdb.connect(
 )
 cursor = connection.cursor()
 while True:
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    sock.bind((UDP_IP, UDP_PORT))
+    sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+    sock.connect((UDP_IP, UDP_PORT))
     currentTime = time.time()
-    while currentTime-time.time() < 10:
-        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        cursor.execute("""INSERT INTO FCC_DATA.CULTIVATION ( FID, ELEVATION, DISTANCE, HEADING, GPS_QUALITY, ENGINE_LOAD,
-                          ENGINE_POWER, FUEL_USED, SPEED, FIELD_ID, DATETIME, POINT_X, POINT_Y, HASH_CODE )
-                          VALUES {data}""")
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    print(data)
+    #while currentTime-time.time() < 10:
+     #   data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+      #  cursor.execute("""INSERT INTO FCC_DATA.CULTIVATION ( FID, ELEVATION, DISTANCE, HEADING, GPS_QUALITY, ENGINE_LOAD,
+       #                   ENGINE_POWER, FUEL_USED, SPEED, FIELD_ID, DATETIME, POINT_X, POINT_Y, HASH_CODE )
+        #                  VALUES """ +str(data))
 
 connection.close()
