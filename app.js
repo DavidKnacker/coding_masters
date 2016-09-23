6077c74323d64d121a0d9d686cd006576e06d5dd
sapevent <sapevent@fair.sap.corp>
2016-09-23 12:19:52 +0200

    bkla\

--- app.js
+++ app.js
@@ -18,21 +18,46 @@ client.connect(function (err) {
   }
 });
 
-app.get('/task', function (req, res) {
+app.get('/task2', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
-      client.exec('select * from DUMMY', function (err, rows) {
-        client.end();
+      query = 'select Count(*) from (SELECT DISTINCT FIELD_ID from FCC_DATA.HARVEST WHERE HOUR(DATETIME) in (' +req.query.hour + '))'
+      console.log( query );
+      client.exec(query, function (err, rows) {
         if (err) {
           return console.error('Execute error:', err);
         }
-        console.log('Results:', rows);
+        res.end('{"solution": {"count": '+rows[0]["COUNT(*)"]+'}}')
       });
-      res.end(  "Some output :)" );
+   });
+})
+app.get('/task3', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      substring = String(req.query.substring)
+      console.log(substring)
+      query = `select distinct FIELD_ID from FCC_DATA.Furrowing where FIELD_ID like '%${substring}%'
+      UNION(select distinct FIELD_ID from FCC_DATA.Harvest where FIELD_ID like '%${substring}%')
+      UNION(select distinct FIELD_ID from FCC_DATA.Harvest_Daily where FIELD_ID like '%${substring}%')
+      UNION(select distinct FIELD_ID from FCC_DATA.Cropping where FIELD_ID like '%${substring}%')
+      UNION(select distinct FIELD_ID from FCC_DATA.Cultivation where FIELD_ID like '%${substring}%')
+      UNION(select distinct FIELD_ID from FCC_DATA.Fertilizer where FIELD_ID like '%${substring}%')
+      order by FIELD_ID asc`
+      console.log( query );
+      client.exec(query, function (err, rows) {
+        if (err) {
+          return console.error('Execute error:', err);
+        }
+        var fields = []
+        for (var item in rows)
+        {
+          fields.push(String(rows[item]['FIELD_ID']))
+        }
+        var json = {"solution": fields}
+        res.end(JSON.stringify(json))
+      });
    });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
 
